10d5b0aa0322127ab661cc30ab6f4be3b7df2ba0
Iván <ivanqgervas@gmail.com>
2021-10-04 13:03:37 +0200

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Form from '../Form'
+import ListaPokemon from '../ListaPokemon'
+
+const Main = () => {
+  return (
+    <main>
+      <Form />
+      <ListaPokemon />
+    </main>
+  );
+};
+
+export default Main;
